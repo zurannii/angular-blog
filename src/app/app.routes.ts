f14d1bc72d20fa947ecac9b07586b289499759d4
Rob Nascimento <zuranmrs2@gmail.com>
2025-03-21 11:20:29 -0300

    renderizando paginas para p vercel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,21 @@ import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
 
 export const routes: Routes = [
-   { path: '',
-    component: HomeComponent
+    {
+        path: '',
+        component: HomeComponent
     },
     {
         path: 'content/:id',
-        component: ContentComponent
+        component: ContentComponent,
+        data: {
+            getPrerenderParams: () => [
+                { id: '1' },
+                { id: '2' },
+                { id: '3' }
+            ]
+        }
     }
-
 ];
+
 
